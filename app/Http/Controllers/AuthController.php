142f78151e9c9837ed07e78233812a46d1b4fe74
kipla <kiplagatbrian18@gmail.com>
2023-09-02 09:45:29 +0300

    Added password

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -33,6 +33,29 @@ class AuthController extends Controller
             return $this->errorResponse('This account already exists. Please Register a different Email', 400, false);
 
         }
+        //check email
+        if (!filter_var($params->email, FILTER_VALIDATE_EMAIL)) {
+            return $this->errorResponse('Please provide a valid email', 400, false);
+        }
+        //check password
+        $check = $this->checkPassword($params->password);
+        if (!$check['valid']) {
+            return $this->errorResponse($check['reason'], 400, false);
+        }
+        //hash password
+        //there should be a backup of this incase the env file is missing this value
+        $getSecret = empty(env('APP_KEY')) ? 'base64:UaY73KVgGOa99uFjacq29YLXZBqFGBund7YLahdDA5A=' : env('APP_KEY');
+        $passwordHash = hash("sha256", trim($params->password) . $getSecret);
+
+        //save the details
+        $store = [
+            'email' => trim($params->email),
+            'password' => $passwordHash,
+            'wallet_id'=> md5(uniqid())
+
+
+        ];
+        Wallet::insert($store);
 
 
         return $this->successResponse('You have successfully registered to the platform', 200, true);
@@ -41,6 +64,20 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         return $this->successResponse('Ok sucess', 200, true);
+    }
+
+    public function checkPassword($password): array
+    {
+        if (strlen($password) < 6) {
+            return ['valid' => false, 'reason' => 'Your Password must be at least 6 characters long'];
+
+        } elseif (preg_match('/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{6,16}$/m', $password) == 0) {
+            return ['valid' => false, 'reason' => 'Your password should be at least 6 characters long with one uppercase letter,one lowercase letter,one special character, and one numeral'];
+        } else {
+            return ['valid' => true, 'reason' => 'Password ok'];
+
+        }
+
     }
 
 }
